cefd6cc8129edcc1196c3716765c0b8d5d2a07eb
UJ5 <ujjawalmodamwal325@gmail.com>
2021-09-27 00:23:59 +0530

    updated files

--- javascript.js
+++ javascript.js
@@ -1,5 +1,5 @@
 let myLibrary = [];
-let idx = 0;
+let index = 0;
 
 
 function createForm(){
@@ -32,48 +32,49 @@ function userData(){
 
 
 function addBooktoLibrary (newBook) {
-	myLibrary[idx] = newBook;
-	idx = idx+1;
-	var bs = document.getElementById('book_shelve');
-	bs.innerHTML ="";
+	myLibrary[index] = newBook;
+	index = index+1;
+	var bookShelve = document.getElementById('book_shelve');
+	bookShelve.innerHTML ="";
 	for(var i = 0, length1 = myLibrary.length; i < length1; i++){
-		newTab(myLibrary[i], i);
+		createNewTab(myLibrary[i], i);
 	}
 }
 
-function newTab(bookObject, idx){
-	console.log(bookObject.name);
+function createNewTab(bookObject, index){
+	
+	//creating the outermost book tab
 	var book =document.createElement('div');
-	book.id = idx;
+	book.id = index;
 	book.className = 'bookShell';
 	document.getElementById('book_shelve').appendChild(book);
-	var read = '<div id = read'+idx+'  class = "readStatus" onClick = "isRead()">Hello!</div>'
+
+
+	//creating the book read status button
+	var read = '<div id = read'+idx+'  class = "notRead" onClick = "isRead()">Hello!</div>'
 	book.innerHTML = read;
+	
+	//creating the book's front cover
 	var div= document.createElement('div');
 	div.className = 'book_info';
-    div.id = 'i'+idx;
-	document.getElementById(idx).insertBefore(div, document.getElementById('read'+idx));
-	var tab = document.getElementById('i'+idx);
+    div.id = 'i'+index;
+	document.getElementById(index).insertBefore(div, document.getElementById('read'+index));
+	var tab = document.getElementById('i'+index);
 	tab.innerHTML = bookObject.info();
 
-	///var read = document.createElement('div');
-	///read.className = 'readStatus';
-	//read.id = 'read'+idx;
-	///read.textContent = 'Hello';
-	//read.onclick = function(){isRead(this.id)};
 }
 
 
+//function to toggle the read status
 function isRead(){
 	var read = document.getElementById(event.srcElement.id);
-	var  color=window.getComputedStyle(read).backgroundColor ;
-	console.log(color);
-	if(color  == 'r'){
-		read.style.backgroundColor = 'green';
-		read.innerHTML = 'Completed';
+
+	if(read.className == "notRead"){
+		read.className = "Read";
+		read.textContent = "Completed";
 	}
-	else if(color == 'g'){
-		read.style.backgroundColor = 'red';
-		read.innerHTML = 'Due';
+	else if(read.className == "Read"){
+		read.className = "notRead";
+		read.textContent = "Due"
 	}
 }
